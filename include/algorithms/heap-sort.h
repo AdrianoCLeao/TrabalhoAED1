d4d59c929189e53707158a824dd70e5347b58548
Miguel Bertol <miguelbertolcodes@gmail.com>
2024-12-10 02:16:21 -0300

    feat: add heap sort for a vector of numbers

--- include/algorithms/heap-sort.h
+++ include/algorithms/heap-sort.h
@@ -1,4 +1,7 @@
 #ifndef HEAP_SORT_H
 #define HEAP_SORT_H
 
+void criaHeapVetorNumeros(int* vetor, int inicio, int fim);
+void heapSortVetorNumeros(int* vetor, int tamanho);
+
 #endif
--- src/algorithms/heap-sort.c
+++ src/algorithms/heap-sort.c
@@ -1,1 +1,39 @@
 #include "../include/algorithms/heap-sort.h"
+
+void criaHeapVetorNumeros(int* vetor, int inicio, int fim) {
+    int maior = inicio;
+    int esquerda = 2 * inicio + 1;
+    int direita = 2 * inicio + 2;
+
+    if (esquerda < fim && vetor[esquerda] > vetor[maior]) {
+        maior = esquerda;
+    }
+
+    if (direita < fim && vetor[direita] > vetor[maior]) {
+        maior = direita;
+    }
+
+    if (maior != inicio) {
+        int temp = vetor[inicio];
+        vetor[inicio] = vetor[maior];
+        vetor[maior] = temp;
+
+        criaHeapVetorNumeros(vetor, maior, fim);
+    }
+}
+
+void heapSortVetorNumeros(int* vetor, int tamanho) {
+    if (!vetor || tamanho < 2) return;
+
+    for (int i = tamanho / 2 - 1; i >= 0; i--) {
+        criaHeapVetorNumeros(vetor, i, tamanho);
+    }
+
+    for (int i = tamanho - 1; i > 0; i--) {
+        int temp = vetor[0];
+        vetor[0] = vetor[i];
+        vetor[i] = temp;
+
+        criaHeapVetorNumeros(vetor, 0, i);
+    }
+}
